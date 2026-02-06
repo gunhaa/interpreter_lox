2ada7eb251fd89366e5619a90af3ec0ddbd253de
gunhaa <wh8299@gmail.com>
2026-02-06 20:42:54 +0900

    environment 추가, 오류 발생으로 디버깅 필요

--- src/main/java/interpreter/lox/Interpreter.java
+++ src/main/java/interpreter/lox/Interpreter.java
@@ -3,6 +3,9 @@ package interpreter.lox;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+    private Environment environment = new Environment();
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
 
@@ -94,6 +97,12 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
         return null;
     }
 
@@ -118,6 +127,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         // 실행되지 않는다
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
