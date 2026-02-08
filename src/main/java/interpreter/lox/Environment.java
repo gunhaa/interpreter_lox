6c92d9de13bd010fb13e747c2ea660b61d19b7f9
gunhaa <wh8299@gmail.com>
2026-02-08 12:54:43 +0900

    문장과 상태 완

--- src/main/java/interpreter/lox/Environment.java
+++ src/main/java/interpreter/lox/Environment.java
@@ -4,25 +4,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    public Environment() {
+        this.enclosing = null;
+    }
+
+    public Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     Object get(Token name) {
         if(values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
 
+        if(enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    // 할당 - 변수의 값을 찾아서 넣는다
     void assign(Token name, Object value) {
         if(values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
         }
 
+        if(enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    // 정의 - 변수에 값을 넣는다
     void define(String name, Object value) {
         values.put(name, value);
     }
